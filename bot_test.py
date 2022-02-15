575e98872eff7d64494a0ce0bbd4e8022cd79355
Patrick <pat_pi@yahoo.com>
2022-02-15 22:21:46 +0100

    bot in progress

--- bot_test.py
+++ bot_test.py
@@ -1,10 +1,15 @@
+import time
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def cookie_down(driver):
-    btn_cookies = driver.find_element(By.CSS_SELECTOR, 'div[class$="summary-buttons"] button[size="large"]:last-child')
+def cookie_down(driver_):
+    btn_cookies = driver_.find_element(By.CSS_SELECTOR, 'div[class$="summary-buttons"] button[size="large"]:last-child')
     btn_cookies.click()
 
 
@@ -24,9 +29,40 @@ pax_infant = '0'
 url = f'https://www.esky.pl/flights/select/{trip_type}/{departure_type}/{departure}/{arrival_type}/{arrival}' \
       f'?departureDate={departure_date}&returnDate={arrival_date}&pa={pax_adult}&py={pax_young}&pc={pax_children}&pi={pax_infant}&sc={flight_standard}'
 
-driver = webdriver.Chrome('./drivers/chromedriver')
+options = Options()
+options.headless = True
+
+service = Service('./drivers/chromedriver')
+service.start()
+driver = webdriver.Remote(service.service_url, options=options)
+# driver.implicitly_wait(10)
+wait = WebDriverWait(driver, 10)
+
 driver.get(url)
 
 cookie_down(driver)
 
-# driver.close()
+# time.sleep(30)
+
+
+# def select_progress_bar(driver_):
+#     progress_bar = driver_.find_element(
+#         by=By.CSS_SELECTOR,
+#         value="div progress-bar"
+#     )
+#     value = progress_bar.text
+#     return not bool(value)
+#
+#
+# wait.until(select_progress_bar)
+
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.badge .content')))
+
+amounts = []
+flights = driver.find_elements(By.CLASS_NAME, 'flight-content')
+for flight in flights:
+    amounts.append(flight.find_element(By.CLASS_NAME, 'amount').text)
+
+print(amounts)
+
+driver.close()
